4975714e940672b47e3de87a4f1925dc3450c976
Dustin Campbell <dustin@teamcampbell.org>
2016-09-13 11:31:28 -0700

    Update command to populate diagnostic collection

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,30 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let collection = vscode.languages.createDiagnosticCollection('perf-text');
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "diagnostic-perf-test" is now active!');
+    let disposable = vscode.commands.registerCommand('extension.populateDiagnostics', () => {
+        let newDiagnostics: [vscode.Uri, vscode.Diagnostic[]][] = [];
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+        for (let i = 1; i <= 2000; i++) {
+            let uri = vscode.Uri.file(`file${i}.txt`);
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+            let entries: vscode.Diagnostic[] = [];
+            for (let j = 1; j <= 1; j++) {
+                let range = new vscode.Range(j-1, 1, j-1, 1);
+                let message = `Diagnostic ${j}`;
+                let diagnostic = new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error);
+                entries.push(diagnostic);
+            }
+
+            newDiagnostics.push([uri, entries]);
+        }
+
+        collection.set(newDiagnostics);
     });
 
     context.subscriptions.push(disposable);
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
+    context.subscriptions.push(collection);
 }
